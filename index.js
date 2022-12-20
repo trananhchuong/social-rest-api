a4e6282f94e7a4819004354d093cc39f04592db8
chuong tran <chuongninolxag@gmail.com>
2022-12-20 14:09:46 +0700

    add post api

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
-// const postRoute = require("./routes/posts");
+const postRoute = require("./routes/posts");
 
 dotenv.config();
 
@@ -29,3 +29,5 @@ app.listen(8800, () => {
 
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
+
